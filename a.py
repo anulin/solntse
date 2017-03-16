08dc9fa624516c7f08af69c31e0f8a4b3d65c591
anulin <anulin@mail.ru>
2017-03-16 19:36:49 +0400

    Add files via upload

---
+++ a.py
@@ -0,0 +1,16 @@
+def al(j,k,A,B):
+    for i in A[k]:
+        if i==j:
+            return(j)
+        B=
+        if k==al(k,i,A,B):
+        return(None)
+    else return(al())
+s=[int(i) for i in input().split()]
+A=[[] for n in range(s[0])]
+B=[0 for n in range(s[0])]
+print(B)
+for j in range(s[1]):
+    c=input().split()
+    A[int(c[0])].append(int(c[1]))
+print()
